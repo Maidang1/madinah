e79255ddb48cf6d9c813609af104d20d0f1f71f4
maidang1 <497350746@qq.com>
2024-10-17 01:20:42 +0800

    fix: add signature

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,7 +1,6 @@
 import { Link } from '@remix-run/react';
-import { useEffectOnce, useLocalStorage } from "react-use"
-
-
+import { useEffectOnce, useLocalStorage } from 'react-use';
+import { Name } from './name';
 
 const tabs = [
   {
@@ -83,24 +82,32 @@ const Header = () => {
     });
   };
   return (
-    <div
-      className='flex justify-between px-8 py-4 fixed left-0 right-0 z-50 dark:bg-[rgb(24,23,23)] bg-white'
-    >
+    <div className='flex justify-between px-8 py-4 fixed left-0 right-0 z-50 dark:bg-[rgb(24,23,23)] bg-white'>
       <div className=''>
-        <a href='/' className='flex gap-x-2 items-center text-[#3c3c43] dark:text-[#fffffff2]'>
-          <span>Madinah</span>
-          <img src="https://avatars.githubusercontent.com/u/50993231?v=4" alt="avatar" className='w-6 h-6 rounded' />
-        </a>
+        <Link
+          to='/'
+          className='flex gap-x-2 items-center text-[#3c3c43] dark:text-[#fffffff2]'
+        >
+          <span>
+            <Name />
+          </span>
+          <img
+            src='https://avatars.githubusercontent.com/u/50993231?v=4'
+            alt='avatar'
+            className='w-6 h-6 rounded'
+          />
+        </Link>
       </div>
       <div className='flex items-center'>
         {tabs.map((item) => (
-          <div
-            key={item.text}
-            className='mr-4 last:mr-0'
-          >
-            <Link to={item.link}>{
-              <span className='text-[#3c3c43] dark:text-[#fffffff2]'>{item.text}</span>
-            }</Link>
+          <div key={item.text} className='mr-4 last:mr-0'>
+            <Link to={item.link}>
+              {
+                <span className='text-[#3c3c43] dark:text-[#fffffff2]'>
+                  {item.text}
+                </span>
+              }
+            </Link>
           </div>
         ))}
         <div
