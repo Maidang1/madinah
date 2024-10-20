cf93f6f52e4748ff8736073975d38ec136f4685b
maidang1 <497350746@qq.com>
2024-10-21 02:06:51 +0800

    feat: update rss link

--- app/routes/[rss.xml].tsx
+++ app/routes/[rss.xml].tsx
@@ -5,9 +5,10 @@ import { generateRssFeed } from '../../utils/rss.server';
 // eslint-disable-next-line import/no-unresolved
 // @ts-expect-error
 import { list } from 'virtual:blog-list';
+import { list as rustList } from 'virtual:rust-list';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const rss = generateRssFeed(list);
+  const rss = generateRssFeed(list.concat(rustList));
 
   return new Response(rss, {
     headers: {
